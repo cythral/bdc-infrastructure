ae5ab2480b9008323c38df2e958aea386d47e059
Talen Fisher <talen.fisher@cythral.com>
2020-04-16 10:02:48 -0500

    BDC-4 Tag Resources

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -6,7 +6,6 @@ for file in $(ls ${cwd}/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
     
-    config={}
-    config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
+    config=$(cat $cwd/config.json | jq --argjson params "$params" '.Parameters=$params')
     echo $config > bdc-infrastructure.${envName}.config.json
 done
